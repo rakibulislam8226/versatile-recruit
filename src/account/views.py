617c9985a04d6b1307c454c02bab9b0f4a1bb032
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-06-26 16:14:28 +0600

    [ADDED] make password hash for custom user

--- src/account/views.py
+++ src/account/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
+from django.contrib.auth.hashers import make_password
 
 from .serializers import CustomUserSerializer
 
@@ -11,6 +12,10 @@ from .serializers import CustomUserSerializer
 def register_user(request):
     serializer = CustomUserSerializer(data=request.data)
     if serializer.is_valid():
+        validated_data = serializer.validated_data
+        password = validated_data['password']
+        hashed_password = make_password(password)  # Hash the password
+        validated_data['password'] = hashed_password
         user = serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
